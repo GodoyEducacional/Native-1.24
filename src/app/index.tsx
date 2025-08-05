07df8bd8679f65b6bbc7800ca05b95aa6136df72
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-08-04 22:13:47 -0300

    3

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,15 @@
 import { View, Text } from "react-native";
+import { Header } from "@/components/header";
+import { CategoryButton } from "@/components/category-button";
 
 export default function Home() {
   return (
     <View className="flex-1 pt-8">
-      <Text className="text-white text- text-2xl">Olá Mundo</Text>
+      <Header title="Faça um pedido" cartQuantityItem={0} />
+
+      <CategoryButton title="Lanche do dia" />
+      <CategoryButton title="Lanche do mês" />
+      <CategoryButton title="Lanche do ano" />
     </View>
   );
 }
