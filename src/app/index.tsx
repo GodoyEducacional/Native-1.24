3c90dd5a1bc06e71c3c2bb50efe5eb10ef3f319e
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-08-08 21:30:11 -0300

    Menu de opções

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,38 @@
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import { Header } from "@/components/header";
 import { CategoryButton } from "@/components/category-button";
 
+import { CATEGORIES } from "@/utils/data/products";
+
+import { useState } from "react";
+
 export default function Home() {
+
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+  }
+
   return (
     <View className="flex-1 pt-8">
-      <Header title="Faça um pedido" cartQuantityItem={0} />
+      <Header title="Faça um pedido" />
 
-      <CategoryButton title="Lanche do dia" />
-      <CategoryButton title="Lanche do mês" />
-      <CategoryButton title="Lanche do ano" />
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
   );
 }
